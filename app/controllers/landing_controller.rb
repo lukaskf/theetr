6019631541a793e23450c58d15bb49cf8e3e3068
Lukas K-F <lukaskf@gmail.com>
2021-06-01 18:43:33 -0500

    Landing page, styling

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,11 @@
 class LandingController < ApplicationController
 
   def index
+    @showtimes = Showtime.all.sort_by(&:start_time).group_by{ |showtime| showtime.start_time.to_date}.transform_values do |shows|
+      shows.group_by{ |showtime| showtime.movie.title }
+    end
+
+    Rails.logger.info @showtimes
   end
 
 end
